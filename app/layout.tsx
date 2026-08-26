b8e03428b05f978278b3d333555ce5825ef14ec6
capibaramusic <capibaramusicbcn@gmail.com>
2026-08-26 15:42:57 +0200

    update 02 2026

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,40 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: contentByLocale[defaultLocale].metadata.title,
   description: contentByLocale[defaultLocale].metadata.description,
+  icons: {
+    icon: [
+      {
+        url: "/icons/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/icons/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/icons/favicon-48x48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+      {
+        url: "/icons/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icons/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: {
+      url: "/icons/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
 };
 
 export default function RootLayout({
